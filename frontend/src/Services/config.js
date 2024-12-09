72453142910f658eacdba6b2005a4e9f6a854fd6
Diego Fernández <judifede217@gmail.com>
2024-12-09 16:13:47 +0000

    Fix BASE_URL

--- frontend/src/Services/config.js
+++ frontend/src/Services/config.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const BASE_URL = import.meta.env.VITE_APP_ENV === 'production'
-  ? 'https://pt-generacion-texto-ia-backend.vercel.app/'
+  ? 'https://pt-generacion-texto-ia-backend.vercel.app'
   : 'http://localhost:3000'
 
 const api = axios.create({
