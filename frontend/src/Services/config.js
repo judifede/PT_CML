864cbb2c4183562e3f426f41712d1f46e95a96ff
Diego Fernández <judifede217@gmail.com>
2024-12-09 13:11:23 +0000

    APP_ENV

--- frontend/src/Services/config.js
+++ frontend/src/Services/config.js
@@ -1,12 +1,12 @@
 
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.NODE_ENV === 'production' 
-  ? 'https://pt-generacion-texto-ia-backend.vercel.app/' 
+const BASE_URL = import.meta.env.APP_ENV === 'production'
+  ? 'https://pt-generacion-texto-ia-backend.vercel.app/'
   : 'http://localhost:3000'
 
 const api = axios.create({
-    baseURL: BASE_URL
+  baseURL: BASE_URL
 })
 
 export default api
